1918327dfacd28338967034f7e27ade6660bfd58
MariamJamal32 <mariamjamal32@gmail.com>
2020-04-07 13:02:09 +0500

    initial commit.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:alpine as builder
+
+WORKDIR /app
+
+COPY package.json .
+RUN npm install
+COPY . .
+
+# do not need to perform volume mappings as in prod, there will be no changes inside the base code
+RUN npm run build 
+
+# from statements are markers of starting a new phase/step in building a project
+FROM nginx
+
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hi I am Mariam! I am stressed! Finally it is working!
         </p>
         <a
           className="App-link"
